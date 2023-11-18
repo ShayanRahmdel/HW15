d76ab72ecfc79f656b8a3ed0c327c99f699dfd5a
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-18 16:27:51 +0330

    create PersonBaseEntity

---
+++ src/main/java/base/entity/PersonBaseEntity.java
@@ -0,0 +1,38 @@
+package base.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class PersonBaseEntity<ID extends Serializable> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @Column(nullable = false,length = 10,unique = true)
+    private String userName;
+    @Column(nullable = false,length = 8)
+    private String password;
+    @Temporal(TemporalType.DATE)
+    private Date dob;
+    @Email
+    private String email;
+    @Max(value = 80,message = "Your age greater than our rules")
+    @Min(value = 15,message = "Your age less than our rules")
+
+    @Column(length = 1,nullable = false)
+    private Character gender;
+}
