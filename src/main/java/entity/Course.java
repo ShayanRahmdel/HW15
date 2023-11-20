5062d27e2236f45b78c18d2d96d5bcd266e24ddb
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-20 11:57:38 +0330

    Create Course

--- src/main/java/entity/Course.java
+++ src/main/java/entity/Course.java
@@ -1,4 +1,23 @@
 package entity;
 
-public class Course {
+import base.entity.CourseBaseEntity;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Course extends CourseBaseEntity<Integer> {
+    @Temporal(value = TemporalType.DATE)
+    private Date courseRelease;
+    @Column(length = 30)
+    private String explainCourse;
 }
