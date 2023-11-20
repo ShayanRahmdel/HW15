fa0b964d37f210921d228838c25062277dc73551
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-20 11:57:29 +0330

    Create ChosenCourse

--- src/main/java/entity/ChosenCourse.java
+++ src/main/java/entity/ChosenCourse.java
@@ -1,4 +1,29 @@
 package entity;
 
-public class ChosenCourse {
+import base.entity.CourseBaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ChosenCourse extends CourseBaseEntity<Integer> {
+
+    @Max(value = 20,message = "Your score greater than limit")
+    @Min(value = 0,message = "Your sore less than limit")
+    private Double score;
+
+
+    @ManyToOne
+    private Master master;
+
+
+    private Boolean isPassed;
 }
