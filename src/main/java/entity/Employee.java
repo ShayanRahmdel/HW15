61c046dec6be733d00007f52c6cd5d7db07a1b4e
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-20 11:58:45 +0330

    add setter and getter

--- src/main/java/entity/Employee.java
+++ src/main/java/entity/Employee.java
@@ -1,14 +1,20 @@
 package entity;
 
 import base.entity.PersonBaseEntity;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
-public class Employee extends PersonBaseEntity<Long> {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee extends User  {
     @Column(unique = true,length = 6,nullable = false)
     private String employeeNumber;
 
-    private Double salary = 10000.0;
+    private Double salary = 700000.0;
 }
