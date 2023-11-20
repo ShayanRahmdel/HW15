28ab8ec2a1491206901149f0f7e25d5d56b9af24
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-20 12:15:15 +0330

    add setter and getter

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -1,14 +1,37 @@
 package entity;
 
 import base.entity.PersonBaseEntity;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
 
 @Entity
-public class Student extends PersonBaseEntity<Long> {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Student extends User {
         @Column(unique = true,length = 6,nullable = false)
         private String studentNumber;
+
+
+        @Column(nullable = false)
+        @Max(value = 20,message = "Your GPA Greater than 20")
+        @Min(value = 12,message = "Your GPA less than 12")
+        private Double lastGPA;
+
+
+        @ManyToMany
+        private List<ChosenCourse> chosenCourses;
+
+
+
 
 
 }
