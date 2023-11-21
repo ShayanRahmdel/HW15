c3ed3035dc0b5ae722ce5ead14eaf2c2fa508c27
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-21 11:38:54 +0330

    create constrotur

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -3,11 +3,12 @@ package entity;
 import base.entity.PersonBaseEntity;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -17,21 +18,41 @@ import java.util.List;
 @NoArgsConstructor
 @ToString
 public class Student extends User {
-        @Column(unique = true,length = 6,nullable = false)
-        private String studentNumber;
-
-
-        @Column(nullable = false)
-        @Max(value = 20,message = "Your GPA Greater than 20")
-        @Min(value = 12,message = "Your GPA less than 12")
-        private Double lastGPA;
-
-
-        @ManyToMany
-        private List<ChosenCourse> chosenCourses;
+    @Column(unique = true, length = 6, nullable = false)
+    private String studentNumber;
 
 
 
 
+
+
+    @ManyToMany
+    private List<ChosenCourse> chosenCourses;
+
+    @OneToMany(mappedBy = "student",cascade = CascadeType.ALL)
+    private List<ChosenCourse> chosenCourse;
+
+    @OneToMany(mappedBy = "student",cascade = CascadeType.ALL)
+    private List<Term> terms;
+
+
+    public Student(@NotNull String firstName, @NotNull String lastName, String userName, String password, Date dob,
+                   @Email String email, @Max(value = 80, message = "Your age greater than our rules")
+                   @Min(value = 15, message = "Your age less than our rules") Integer age, String city, Character gender, String studentNumber) {
+        super(firstName, lastName, userName, password, dob, email, age, city, gender);
+        this.studentNumber = studentNumber;
+
+    }
+
+    public Student(Integer id, @NotNull String firstName, @NotNull String lastName, String userName, String password
+            , Date dob, @Email String email, @Max(value = 80, message = "Your age greater than our rules")
+                   @Min(value = 15, message = "Your age less than our rules") Integer age, String city, Character gender, String studentNumber
+            ) {
+        super(firstName, lastName, userName, password, dob, email, age, city, gender);
+        this.setId(id);
+        this.studentNumber = studentNumber;
+
+    }
 
 }
+
