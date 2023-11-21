1dcd6abdfb764a9b1b2f2cbd812f2bb29d976f44
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-21 11:33:28 +0330

    add counstructor

--- src/main/java/entity/Course.java
+++ src/main/java/entity/Course.java
@@ -3,11 +3,11 @@ package entity;
 import base.entity.CourseBaseEntity;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Setter
@@ -20,4 +20,13 @@ public class Course extends CourseBaseEntity<Integer> {
     private Date courseRelease;
     @Column(length = 30)
     private String explainCourse;
+    @OneToMany(mappedBy = "course",cascade = CascadeType.ALL)
+    private List<ChosenCourse> ChosenCourse;
+
+
+    public Course( String name, @Min(value = 1, message = "We dont have less than 1 unit") @Max(value = 3, message = "We dont have greater than 3 unit") Integer unit, Date courseRelease, String explainCourse) {
+        super( name, unit);
+        this.courseRelease = courseRelease;
+        this.explainCourse = explainCourse;
+    }
 }
