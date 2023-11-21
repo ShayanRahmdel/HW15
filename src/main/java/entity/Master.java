0b41004e34f4de280b389596d634f042ceb014d5
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-21 11:36:22 +0330

    add constructor

--- src/main/java/entity/Master.java
+++ src/main/java/entity/Master.java
@@ -4,6 +4,11 @@ import base.entity.PersonBaseEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -19,6 +24,17 @@ public class Master extends User {
     private MasterType masterType;
 
     private Double salary= 1000000.0;
+
     @OneToMany(mappedBy = "master",cascade = CascadeType.ALL)
     private List<ChosenCourse> chosenCourses;
+
+    @OneToMany(mappedBy = "master",cascade = CascadeType.ALL)
+    private List<Term> terms;
+
+    public Master(@NotNull String firstName, @NotNull String lastName, String userName, String password, Date dob, @Email String email, @Max(value = 80, message = "Your age greater than our rules") @Min(value = 15, message = "Your age less than our rules") Integer age, String city, Character gender, String masterNumber, MasterType masterType) {
+        super(firstName, lastName, userName, password, dob, email, age, city, gender);
+        this.masterNumber = masterNumber;
+        this.masterType = masterType;
+
+    }
 }
