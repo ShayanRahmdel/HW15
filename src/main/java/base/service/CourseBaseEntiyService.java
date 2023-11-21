ef0d6282cc3a9b77de01d9072c3f24a1f201dd40
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-21 11:34:15 +0330

    create CourseBaseEntiyService and CourseBaseEntityServiceImpl and methods

--- src/main/java/base/service/CourseBaseEntiyService.java
+++ src/main/java/base/service/CourseBaseEntiyService.java
@@ -1,4 +1,25 @@
 package base.service;
 
-public interface CourseBaseEntiyService {
+import base.entity.CourseBaseEntity;
+import base.entity.PersonBaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CourseBaseEntiyService <T extends CourseBaseEntity<ID>,ID extends Serializable>{
+    T saveOrUpdate(T entity);
+
+    Optional<T> findById(ID id);
+
+    void deleteById(ID id);
+
+    Collection<T> findAll();
+
+    void beginTransaction();
+
+    void commitTransaction();
+
+    void rollback();
+
 }
