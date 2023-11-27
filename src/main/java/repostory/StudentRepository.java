c248523da41d03a64415bf20336ed51c2a7959d3
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-27 12:37:53 +0330

    Refactor StudentRepository

--- src/main/java/repostory/StudentRepository.java
+++ src/main/java/repostory/StudentRepository.java
@@ -1,9 +1,11 @@
 package repostory;
 
-import base.repository.PersonBaseEntityRepository;
+import base.repository.BaseEntityRepository;
+import entity.Employee;
 import entity.Student;
 
-public interface StudentRepository extends PersonBaseEntityRepository<Student,Integer> {
+public interface StudentRepository extends BaseEntityRepository<Student,Integer> {
 
-
+    Student findByUserNameAndPasswordReturn(String userName, String password);
+    Boolean findByUserNameAndPassword(String userName, String password);
 }
--- src/main/java/repostory/impl/StudentRepostoryImpl.java
+++ src/main/java/repostory/impl/StudentRepostoryImpl.java
@@ -1,12 +1,14 @@
 package repostory.impl;
 
-import base.repository.impl.PersonBaseEntityRepositoryImpl;
+import base.repository.impl.BaseEntityRepositoryImpl;
+import entity.Employee;
 import entity.Student;
 import repostory.StudentRepository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
-public class StudentRepostoryImpl extends PersonBaseEntityRepositoryImpl<Student,Integer> implements StudentRepository {
+public class StudentRepostoryImpl extends BaseEntityRepositoryImpl<Student,Integer> implements StudentRepository {
 
     public StudentRepostoryImpl(EntityManager entityManager){
         this.entityManager=entityManager;
@@ -17,4 +19,21 @@ public class StudentRepostoryImpl extends PersonBaseEntityRepositoryImpl<Student
     }
 
 
+    @Override
+    public Student findByUserNameAndPasswordReturn(String userName, String password) {
+        String hql = "Select s FROM Student s WHERE s.userName= :userName and s.password= :password";
+        TypedQuery<Student> query = entityManager.createQuery(hql, Student.class);
+        query.setParameter("userName",userName);
+        query.setParameter("password",password);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Boolean findByUserNameAndPassword(String userName, String password) {
+        String hql = "Select s FROM Student s WHERE s.userName= :userName and s.password= :password";
+        TypedQuery<Student> query = entityManager.createQuery(hql, Student.class);
+        query.setParameter("userName",userName);
+        query.setParameter("password",password);
+        return query.getSingleResult() != null;
+    }
 }
