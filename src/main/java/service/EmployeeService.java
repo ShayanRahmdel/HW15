75dae7d4eefd89991dc2bec866bd108dd41661e1
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-27 12:34:47 +0330

    refactor EmployeeService

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package service;
 
-import base.service.PersonBaseService;
+import base.service.BaseService;
 import entity.Employee;
 
-public interface EmployeeService extends PersonBaseService<Employee,Integer> {
+public interface EmployeeService extends BaseService<Employee,Integer> {
+    Boolean findByUserNameAndPassword(String userName,String password);
+    Employee findByUserNameAndPasswordReturn(String userName,String password);
+    Double showSalary(Integer id);
+
 }
--- src/main/java/service/impl/EmployeeServiceImpl.java
+++ src/main/java/service/impl/EmployeeServiceImpl.java
@@ -1,12 +1,31 @@
 package service.impl;
 
-import base.service.impl.PersonBaseEntityServiceImpl;
+import base.service.impl.BaseEntityServiceImpl;
 import entity.Employee;
 import repostory.EmployeeRepository;
 import service.EmployeeService;
 
-public class EmployeeServiceImpl extends PersonBaseEntityServiceImpl<Employee,Integer, EmployeeRepository> implements EmployeeService {
+public class EmployeeServiceImpl extends BaseEntityServiceImpl<Employee,Integer, EmployeeRepository> implements EmployeeService {
     public EmployeeServiceImpl(EmployeeRepository repository) {
         super(repository);
     }
+
+    @Override
+    public Boolean findByUserNameAndPassword(String userName, String password) {
+        return repository.findByUserNameAndPassword(userName,password);
+    }
+
+    @Override
+    public Employee findByUserNameAndPasswordReturn(String userName, String password) {
+        return repository.findByUserNameAndPasswordReturn(userName,password);
+    }
+
+    @Override
+    public Double showSalary(Integer id) {
+        repository.findById(id);
+        System.out.println();
+        return  repository.showSalary(id);
+    }
+
+
 }
