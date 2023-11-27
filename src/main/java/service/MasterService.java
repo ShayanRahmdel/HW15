95063d1feeb6c675d0c7fa05d113a7b8441cea17
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-11-27 12:35:54 +0330

    refactor MasterService

--- src/main/java/service/MasterService.java
+++ src/main/java/service/MasterService.java
@@ -1,7 +1,7 @@
 package service;
 
-import base.service.PersonBaseService;
+import base.service.BaseService;
 import entity.Master;
 
-public interface MasterService extends PersonBaseService<Master,Integer> {
+public interface MasterService extends BaseService<Master,Integer> {
 }
--- src/main/java/service/impl/MasterServiceImpl.java
+++ src/main/java/service/impl/MasterServiceImpl.java
@@ -1,11 +1,11 @@
 package service.impl;
 
-import base.service.impl.PersonBaseEntityServiceImpl;
+import base.service.impl.BaseEntityServiceImpl;
 import entity.Master;
 import repostory.MasterRepository;
 import service.MasterService;
 
-public class MasterServiceImpl extends PersonBaseEntityServiceImpl<Master,Integer, MasterRepository> implements MasterService {
+public class MasterServiceImpl extends BaseEntityServiceImpl<Master,Integer, MasterRepository> implements MasterService {
     public MasterServiceImpl(MasterRepository repository) {
         super(repository);
     }
